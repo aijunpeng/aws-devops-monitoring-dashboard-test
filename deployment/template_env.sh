6f82d0e7f860b5ecfc78dfff2655a6757fd1c58c
Aijun Peng <penaijun@amazon.com>
2024-04-05 15:07:26 -0400

    added launch.yaml, shell scripts and npm commands to support codecatalyst integration

---
+++ deployment/template_env.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+export SOLUTION_ID="so0134" 
+export SOLUTION_NAME="DevOps Monitoring Dashboard on AWS"  
+export DIST_VERSION="v1.8.8" 
+export DIST_SOLUTION_NAME="aws-devops-monitoring-dashboard"
