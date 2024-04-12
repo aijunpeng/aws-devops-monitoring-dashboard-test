2ffe7030517d0540605f5940c3d367aa3a111550
Aijun Peng <penaijun@amazon.com>
2024-04-12 00:30:34 -0400

    Updated workflow code and relevant files

---
+++ .codecatalyst/workflows/install_dependencies.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+# Get reference for all important folders
+deployment_dir="$PWD"
+cdk_out_dirc="$deployment_dir/../source/cdk.out"
+
+do_cmd () {
+    echo "------ EXEC $*"
+    $*
+    rc=$?
+    if [ $rc -gt 0 ]
+    then
+            echo "Aborted - rc=$rc"
+            exit $rc
+    fi
+}
+
+echo "------------------------------------------------------------------------------"
+echo "[Init] Install dependencies for Lambda functions"
+echo "------------------------------------------------------------------------------"
+cd $cdk_out_dirc
+echo "aijun directory"
+echo $PWD
+for folder in asset.* ; do
+    cd "$folder"
+
+    function_name=${PWD##*/}
+    echo "Installing dependencies for $function_name"
+
+    for temp_folder in ".nyc_output" ".venv-prod" ".venv-test" "__pycache__"; do
+        if [ -d "$temp_folder" ]; then
+            echo "$temp_folder exists, removing it"
+            do_cmd rm -rf $temp_folder
+        fi
+    done
+
+    if [ -e "requirements.txt" ]; then
+        echo "Installing python lambda using virtual environment"
+        python3 -m venv .venv-test
+        echo "Activating virtual environment"
+        source .venv-test/bin/activate
+        python3 -m pip install -U pip setuptools
+        echo "Executing pip3 install -q -r requirements.txt --target ./"
+        python3 -m pip install -q -r requirements.txt --target ./
+        echo "Deactivating virtual environment"
+        deactivate
+        echo "Deleting python virtual environment"
+        rm -fr .venv-test
+    elif [ -e "package.json" ]; then
+        echo "Installing node dependencies"
+        echo "Executing do_cmd npm install --omit=dev"
+        do_cmd npm install --omit=dev
+    fi
+
+    cd ..
+done
+
+# Return to the directory where we started
+cd $deployment_dir
---
+++ .codecatalyst/workflows/set_solution_env.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+export SOLUTION_ID="SO0134"
+export SOLUTION_NAME="DevOps Monitoring Dashboard on AWS"
+export DIST_SOLUTION_NAME="aws-devops-monitoring-dashboard"
+export DIST_VERSION=$LAUNCH_OPTIONS_DIST_VERSION
+echo "SOLUTION_ID: $SOLUTION_ID"
+echo "DIST_VERSION: $DIST_VERSION"
+echo "DIST_SOLUTION_NAME: $DIST_SOLUTION_NAME"
+echo "LAUNCH_OPTIONS_QuickSightPrincipalArn: $LAUNCH_OPTIONS_QuickSightPrincipalArn"
+echo "LAUNCH_OPTIONS_QuickSightSourceTemplateArn: $LAUNCH_OPTIONS_QuickSightSourceTemplateArn"
+echo "LAUNCH_OPTIONS_UseGitHub: $LAUNCH_OPTIONS_UseGitHub"
+echo "LAUNCH_OPTIONS_WebhookSecretToken: $LAUNCH_OPTIONS_WebhookSecretToken"
+echo "LAUNCH_OPTIONS_AllowedIPs: $LAUNCH_OPTIONS_AllowedIPs"
+echo "LAUNCH_OPTIONS_CodeCommitRepo: $LAUNCH_OPTIONS_CodeCommitRepo"
+echo "LAUNCH_OPTIONS_DataDuration: $LAUNCH_OPTIONS_DataDuration"
+echo "LAUNCH_OPTIONS_PrincipalTypepe: $LAUNCH_OPTIONS_PrincipalType"
+echo "LAUNCH_OPTIONS_PrincipalList: $LAUNCH_OPTIONS_PrincipalList"
+echo "LAUNCH_OPTIONS_S3TransitionDays: $LAUNCH_OPTIONS_S3TransitionDays"
+echo "LAUNCH_OPTIONS_TagsConfigCodeCommit: $LAUNCH_OPTIONS_TagsConfigCodeCommit"
+echo "LAUNCH_OPTIONS_TagsConfigCodeBuild: $LAUNCH_OPTIONS_TagsConfigCodeBuild"
+echo "LAUNCH_OPTIONS_TagsConfigCodePipeline: $LAUNCH_OPTIONS_TagsConfigCodePipeline"
